7239041fa1403cf53f1773c9206af233f0763a69
zicolino <167160363+zicolino@users.noreply.github.com>
2025-01-15 15:59:53 -0300

    proyecto final

--- brsana/src/TalleresSlider.jsx
+++ brsana/src/TalleresSlider.jsx
@@ -42,7 +42,7 @@ function TalleresSlider() {
             title="YOGA Y CERAMICA" 
             img1src={"./src/assets/yoga2.jpeg"}
             img2src={"./src/assets/ceramica.jpeg"}
-            img3src={"./src/assets/brunch.jpeg"}
+            img3src={"./src/assets/brunch 2.jpeg"}
             ptext="En este taller disfrutaremos de una clase de Yin yoga muy restaurativa para devolverle al cuerpo la flexibilidad perdida con el estrés laboral luego disfrutaremos un brunch delicioso y saludable para prepararnos los sentidos y expresarnos en la arcilla, llevandonos la pieza que realicemos!" 
             schedule="Día y horario a definir" />
             
@@ -52,9 +52,9 @@ function TalleresSlider() {
           <div className="swiper-slide">
             <TallerCard
             title="YOGA Y FOTOGRAFIA" 
-            img1src={"./src/assets/yoga2.jpeg"}
+            img1src={"./src/assets/yoga4.jpeg"}
             img2src={"./src/assets/yoga-y-foto.jpeg"}
-            img3src={"./src/assets/brunch.jpeg"}
+            img3src={"./src/assets/brunch 4.jpeg"}
             ptext="En este taller unimos nuestras pasiones! el yoga y la fotografía haremos un clase de yoga con plena atención en la alineación de cada asana y en sus beneficios físicos y mentales, pasaremos al  brunch apetitoso y con degustación de vino para así tomar nuestras cámaras y  captar la paz encontrada en nuestra asana preferida. Un plan ideal para realizar bien acompañado." 
             schedule="Día y horario a definir" />
             
@@ -64,21 +64,19 @@ function TalleresSlider() {
           <div className="swiper-slide">
             <TallerCard 
             title="YOGA Y AROMATERAPIA" 
-            img1src={"./src/assets/yoga2.jpeg"}
+            img1src={"./src/assets/yoga5.jpeg"}
             img2src={"./src/assets/aromaterapia.jpeg"}
-            img3src={"./src/assets/brunch.jpeg"}
+            img3src={"./src/assets/brunch 3.jpeg"}
             ptext="Es un doble taller pensado para el relax y un despertar a la armonía interior,  explorando cómo los aromas, las texturas, los colores, modifican nuestro estado de ánimo y nuestras  emociones, un detox mental al estrés y a los agresivos agentes exógenos que nos alejan de nuestro natural equilibrio interior. Finalmente disfrutaremos un saludable brunch con nuestros chakras ya armonizados." 
             schedule="Día y horario a definir" />
-            
-            <div className="swiper-lazy-preloader"></div>
             </div>
             
           <div className="swiper-slide">
             <TallerCard 
             title="YOGA Y ZENTANGLE" 
-            img1src={"./src/assets/yoga2.jpeg"}
+            img1src={"./src/assets/yoga6.jpeg"}
             img2src={"./src/assets/zentangle.jpeg"}
-            img3src={"./src/assets/brunch.jpeg"}
+            img3src={"./src/assets/brunch 1.jpeg"}
             ptext="Taller  diseñado especialmente para  mejorar un estado de ansiedad y estrés, iniciamos con  una clase de Yin Yoga donde las posturas/asanas nos brindaran una relajación física y mental, abriéndonos a un estado de calma para continuar con un brunch saludable así nutrirnos de modo consciente y prepararnos para una meditación artística donde liberar la mente con trazos sobre papel formando patrones aleatorios enfocandonos en una concentración plena" 
             schedule="Día y horario a definir" />
 
@@ -88,9 +86,9 @@ function TalleresSlider() {
             <div className="swiper-slide">
             <TallerCard 
             title="YOGA Y SAHUMOS" 
-            img1src={"./src/assets/yoga2.jpeg"}
+            img1src={"./src/assets/yoga7.jpeg"}
             img2src={"./src/assets/sahumos.jpeg"}
-            img3src={"./src/assets/brunch.jpeg"}
+            img3src={"./src/assets/brunch 5.jpeg"}
             ptext="Este taller nos brinda la posibilidad de armonizar nuestro interior  a través de una clase de yoga que finaliza con una relajación guiada y  así pasar al apetitoso brunch saludable que nos da las energías necesarias para aprender a realizar nuestro sahumo casero y personalizado de acuerdo a nuestra necesidad emocional abordaremos los efectos de las distintas hierbas y sus beneficios. y nos lo llevamos a casa!!" 
             schedule="Día y horario a definir" />
             
