fd8e6ff05d488a89677025fdcbbd55f110b17be7
zicolino <167160363+zicolino@users.noreply.github.com>
2025-01-16 11:01:37 -0300

    config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [], // Aquí puedes agregar otros módulos si fuera necesario
+    },
+  },
   base: '/brunchasana/'
 })
